5c79e6ce8d5ba7232caec0a586bbcce420ef8d7b
Vivek G <vivek.g@browserstack.com>
2025-07-04 17:57:50 +0530

    Fix broken images

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   env: {
     // Sample LINK prefix: /listen
     // Do not add the slash at the last
-    LINK_PREFIX: process.env.NODE_ENV === "production" ? "/wcag-hands-on" : ""
+    NEXT_PUBLIC_LINK_PREFIX: process.env.NODE_ENV === "production" ? "/wcag-hands-on" : ""
   },
 }
 
